93ee8ae1f02485fea5b7110f241d15d2cb9c9180
Kristin <kmyap128@gmail.com>
2025-04-28 11:26:20 -0400

    created playlist model

---
+++ server/models/Playlist.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const _ = require('underscore');
+
+const setName = (name) => _.escape(name).trim();
+
+const PlaylistSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    set: setName,
+  },
+  songs: [{
+    type: mongoose.Schema.ObjectId,
+    ref: 'Song', 
+  }],
+  user: {
+    type: mongoose.Schema.ObjectId,
+    required: true,
+    ref: 'Account', 
+  },
+  createdDate: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+PlaylistSchema.statics.toAPI = (doc) => ({
+  name: doc.name,
+  songs: doc.songs,
+});
+
+const PlaylistModel = mongoose.model('Playlist', PlaylistSchema);
+module.exports = PlaylistModel;
